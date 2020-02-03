3afc83ecf6f2f5dd6e2c0074208568fc46d3650f
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-02-02 23:11:50 -0500

    added a grid to the TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,34 +5,80 @@ import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
+
+import GridList from '@material-ui/core/GridList';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
-    maxWidth: 500
-    // backgroundColor: theme.palette.background.paper
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden'
+    //   backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 400,
+    height: 250,
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)'
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
+  },
+  icon: {
+    color: 'white'
   }
 }));
 
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     width: '100%',
+//     maxWidth: 500
+//     // backgroundColor: theme.palette.background.paper
+//   }
+// }));
+
 export function TodoList(props) {
   const classes = useStyles();
+
   return (
-    <div>
-      {props.state.map(item => {
-        return (
-          <List className={classes.root}>
+    <div className={classes.root}>
+      <GridList cellHeight={'auto'} spacing={0}>
+        {props.state.map(item => {
+          console.log(props.state);
+          return (
             <ListItem button>
-              <ListItemText id={''} />
               <Todo
                 item={item}
                 key={item.id}
                 handleComplete={props.handleComplete}
               />
             </ListItem>
-          </List>
-        );
-      })}
+          );
+        })}
+      </GridList>
     </div>
   );
+
+  //   return (
+  //     <div>
+  //       {props.state.map(item => {
+  //         console.log(props.state);
+  //         return (
+  //           <List key={item.id} className={classes.root}>
+  //             <ListItem button>
+  //               <ListItemText />
+  //               <Todo
+  //                 item={item}
+  //                 key={item.id}
+  //                 handleComplete={props.handleComplete}
+  //               />
+  //             </ListItem>
+  //           </List>
+  //         );
+  //       })}
+  //     </div>
+  //   );
 }
